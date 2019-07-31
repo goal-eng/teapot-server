d4ffa40aaf31c64b6faefb4a9fe303490039153b
goal-eng <goooaldev@gmail.com>
2019-08-01 00:20:02 +0200

    Implementation of brewing `earl-grey` and `english-breakfast` teas and their tests

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ TEA_VARIANTS = [
     'english-breakfast',
     'earl-grey',
 ]
+HIGH_TRAFFIC_VARIANT = 'earl-grey'
 
 
 def create_alternates():
@@ -28,6 +29,7 @@ def create_alternates():
 TEA_ALTERNATES = create_alternates()
 
 # Runtime variables
+POTS_BREWING = {variant: False for variant in TEA_VARIANTS}
 TRAFFIC = weakref.WeakValueDictionary()
 CUR_SECOND_COUNTER = None  # used to keep reference
 
@@ -51,6 +53,10 @@ def increase_traffic_by_request(request, tea_variant):
 
 
 def slash(request):
+    """
+    :type request:
+    """
+
     endpoint = request.match_dict.get('endpoint', '')
 
     if request.method != 'BREW':
@@ -64,20 +70,70 @@ def slash(request):
                 headers={'Alternates': TEA_ALTERNATES}
             )
 
+        # Some pot
         elif endpoint in TEA_VARIANTS:
-            traffic = increase_traffic_by_request(request, endpoint)
 
-            if traffic < MIN_REQUESTS_COUNT:
-                # FIXME: uvloop is unable to return status code 424
-                #        see https://github.com/squeaky-pl/japronto/issues/131
+            # Wrong Content-Type
+            if request.headers.get('Content-Type', '') != TEA_CONTENT_TYPE:
                 return request.Response(
-                    code=424,
-                    text=f'Traffic too low to brew "{endpoint}" tea: {traffic}/{MIN_REQUESTS_COUNT}'
+                    code=400,
+                    headers={'Alternates': TEA_ALTERNATES}
                 )
 
-            else:
-                return request.Response(text='UEA')
+            is_brewing = POTS_BREWING[endpoint]
 
+            # Start brewing
+            if request.body == b'start':
+
+                # Pot is busy - already brewing
+                if is_brewing:
+                    return request.Response(
+                        code=503,
+                        text='Pot is busy'
+                    )
+
+                # Make sure there is enough traffic for high traffic pot
+                if endpoint == HIGH_TRAFFIC_VARIANT:
+                    traffic = increase_traffic_by_request(request, tea_variant=endpoint)
+
+                    if traffic < MIN_REQUESTS_COUNT:
+                        # FIXME: uvloop is unable to return status code 424
+                        #        see https://github.com/squeaky-pl/japronto/issues/131
+                        return request.Response(
+                            code=424,
+                            text=f'Traffic too low to brew "{endpoint}" tea: {traffic}/{MIN_REQUESTS_COUNT}'
+                        )
+
+                # Successfully start brewing
+                POTS_BREWING[endpoint] = True
+
+                return request.Response(
+                    code=202,
+                    text='Brewing'
+                )
+
+            # Stop brewing
+            if request.body == b'stop':
+
+                if not is_brewing:
+                    return request.Response(
+                        code=400,
+                        text='No beverage is being brewed by this pot',
+                    )
+
+                # Successfully stop brewing
+                POTS_BREWING[endpoint] = False
+
+                return request.Response(
+                    code=201,
+                    text='Finished',
+                )
+
+            return request.Response(
+                code=400
+            )
+
+        # Unknown pot
         else:
             return request.Response(
                 code=503,
@@ -96,7 +152,7 @@ r.add_route('/{endpoint}', slash)
 @click.option('--host', default='0.0.0.0')
 @click.option('--port', default='8080')
 @click.option('--worker_num', default=None)
-@click.option('--debug', default=False)
+@click.option('--debug', default=False, is_flag=True)
 def cli(host, port, worker_num, debug):
     click.echo('Starting server with following configuration:')
     click.echo('Host: %r' % host)
